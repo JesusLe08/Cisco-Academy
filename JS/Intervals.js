552d2604f6a267c4ecac4fcf0788333a8838b18f
JesusLe08 <jisusleal15@gmail.com>
2026-05-01 20:25:48 -0600

    Cambios de ultimo lab

--- JS/Intervals.js
+++ JS/Intervals.js
@@ -6,3 +6,20 @@ let IntervalId = setInterval(function () {
 setTimeout(function () {
     clearInterval(IntervalId)
 }, 20000);
+
+let fibbRec = function (n) {
+    let retVal = 0;
+    if (n != 0) {
+        if (n === 1) {
+            retVal = 1;
+        } else {
+            retVal = fibbRec(n - 1) + fibbRec(n - 2);
+        }
+    }
+    return retVal;
+};
+
+let fibb = n => n == 0 ? 0 : (n == 1 ? 1 : fibb(n - 1) + fibb(n - 2));
+
+console.log(fibbRec(4));
+console.log(fibb(4))
---
+++ JS/Lab5.js
@@ -0,0 +1,6 @@
+let a = (n) => {
+    return n > 2 ? n * a(n - 1) : 2
+}
+console.log(a(6));
+
+debugger;
